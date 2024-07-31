60154ee27e8a4074e409068f5a8cafb554145746
Koren Kazoom <65340580+korenka@users.noreply.github.com>
2024-07-31 12:41:32 +0300

    Update Contact.java

--- phonebook-api/src/main/java/com/koren/phonebook_api/model/Contact.java
+++ phonebook-api/src/main/java/com/koren/phonebook_api/model/Contact.java
@@ -6,7 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "contacts")
@@ -17,7 +16,6 @@ public class Contact {
     private Long id;
 
     @Column(name = "first_name")
-    @NotNull(message = "First name is required")
     private String firstName;
 
     @Column(name = "last_name")
